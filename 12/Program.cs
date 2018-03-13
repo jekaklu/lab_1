314b515a02bcb9a9a031e701722d45189aa0eee4
jekaklu <jekaklu@gmail.com>
2018-03-13 10:12:58 +0300

    Replay

--- 12/Program.cs
+++ 12/Program.cs
@@ -31,6 +31,13 @@ namespace S4et
             Console.WriteLine($"Дальность: { this.Range }");
         }
 
+        static void Main(string[] args)
+        {
+            Weapon scr = new Weapon();
+            scr.Input();
+            scr.Print();
+            Console.ReadKey();
+        }
 
     }
 }
