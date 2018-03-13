867f403c35d5de43dcbc3a22c60b6e4364d5a818
jekaklu <jekaklu@gmail.com>
2018-03-13 10:12:21 +0300

    yes

--- 12/Program.cs
+++ 12/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _12
+
+namespace S4et
 {
-    class Program
+    class Weapon
     {
-        static void Main(string[] args)
+        public String Name { get; set; }
+
+        private double Kaliber { get; set; }
+
+        public double Range { get; set; }
+
+
+
+        public void Input()
         {
+            Console.Write("Введите название оружия: "); this.Name = Console.ReadLine();
+            Console.Write("Введите калибр оружия: "); this.Kaliber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите его дальность: "); this.Range = Convert.ToDouble(Console.ReadLine());
         }
+
+        public void Print()
+        {
+            Console.WriteLine($"Название: { this.Name }");
+            Console.WriteLine($"Калибр: { this.Kaliber }");
+            Console.WriteLine($"Дальность: { this.Range }");
+        }
+
+
     }
 }
